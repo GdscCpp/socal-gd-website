f0034240dada7f936f9ef0d51be5145c5095c217
blbergo <bryan.l.bergo@gmail.com>
2024-06-04 19:02:16 -0700

    colors

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="text-white p-4 h-screen w-full flex items-center justify-center bg-dark-400">
+      <h1 className="text-2xl bg-2cg-4 text-transparent inline-block bg-clip-text">
+        Hello, world!
+      </h1>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,60 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "4cg-1":
+          "linear-gradient(to right, #34a853 0%,#4284f5 30%,#4284f5 35%,#9e6cd5 55%,#eb4235 65%,#eb4235 75%,#f46930 80%,#fecb33 90%,#fecb33 100%);",
+        "4cg-2":
+          "linear-gradient(to right, #4284f5 0%,#4284f5 10%,#9e6cd5 25%,#eb4235 35%,#eb4235 35%,#eb4235 45%,#f46930 60%,#fecb33 70%,#fecb33 80%,#34a853 90%,#34a853 100%);",
+        "2cg-1":
+          "linear-gradient(to right, #1e5799 0%,#4284f5 25%,#9e6cd5 50%,#eb4235 75%,#eb4235 100%);",
+        "2cg-2":
+          "linear-gradient(to right, #fbbc04 0%,#fbbc04 25%,#f46930 50%,#eb4235 75%,#eb4235 100%);",
+        "2cg-3":
+          "linear-gradient(to right, #4284f5 0%,#4284f5 25%,#34a853 75%,#34a853 100%);",
+        "2cg-4":
+          "linear-gradient(to right, #fbbc04 0%,#fbbc04 25%,#34a853 75%,#34a853 100%);",
+      },
+      colors: {
+        green: {
+          500: "#34A853",
+        },
+        red: {
+          500: "#EB4235",
+        },
+        blue: {
+          500: "#4284F5",
+        },
+        yellow: {
+          500: "#FBBC04",
+        },
+        danger: {
+          400: "#6E1717",
+          300: "#A92323",
+          200: "#C92929",
+          100: "#E16B6B",
+          50: "#FBEAEA",
+        },
+        success: {
+          400: "#0A4834",
+          300: "#0E674B",
+          200: "#12825F",
+          100: "#1BC590",
+          50: "#F6FEFB",
+        },
+        warning: {
+          400: "#A6620D",
+          300: "#EE9322",
+          200: "#F2AB53",
+          100: "#F9DBB5",
+          50: "#FFFDFA",
+        },
+        dark: {
+          400: "#1E1E1E",
+          300: "#1C1C1C",
+          200: "#1A1A1A",
+          100: "#161616",
+          50: "121212",
+        },
       },
     },
   },
