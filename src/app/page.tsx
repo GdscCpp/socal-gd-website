f71c9950e5e85b340f0c5901d14aab9f7eb80f9d
miloszkryzia1 <milosz.kryzia@gmail.com>
2024-06-14 18:21:30 -0700

    add disabled look to buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Textarea from "@/components/input/Textarea";
 
 export default function Page() {
   return (
-    <div className="text-white p-4 h-screen w-full flex items-center justify-center bg-dark-400">
+    <div className="text-white p-4 h-screen w-full flex items-center justify-center bg-black">
       <h1 className="text-heading-xl bg-2cg-4 text-transparent inline-block bg-clip-text">
         Hello, world!
       </h1>
@@ -18,6 +18,7 @@ export default function Page() {
       <Button
         label="Test LG"
         type="button"
+        disabled={true}
         variant={BUTTON_VARIANTS.SUCCESS_LG}
       />
       <div className="w-5" />
--- src/components/input/Button.tsx
+++ src/components/input/Button.tsx
@@ -1,21 +1,28 @@
-//TODO: Moved reused properties to Button's className
-//Add LG and SM variants
+//TODO:
+//Add disabled states (1/2 done)
 //Add GRADIENT_1 and GRADIENT_2 VARIANTS
-//Add disabled states
+
+//TODO: might need different bg color for disabled buttons
+const DISABLED_STYLE_1 =
+  "disabled:bg-dark-400 disabled:cursor-not-allowed disabled:opacity-40";
+const DISABLED_STYLE_2 = ""; //TODO: Add disabled style for gradient border buttons
+
 export enum BUTTON_VARIANTS {
-  PRIMARY_LG = "bg-blue-500 text-white py-3 px-4 text-title-xl hover:shadow-blue-700 hover:bg-blue-600 active:bg-blue-500",
-  PRIMARY_SM = "bg-blue-500 text-white py-2.5 px-4 text-title-lg hover:shadow-blue-700 hover:bg-blue-600 active:bg-blue-500",
-  DANGER_LG = "bg-danger-200 text-white py-3 px-4 text-title-xl hover:shadow-danger-400 hover:bg-danger-300 active:bg-danger-200",
-  DANGER_SM = "bg-danger-200 text-white py-2.5 px-4 text-title-lg hover:shadow-danger-400 hover:bg-danger-300 active:bg-danger-200",
-  SUCCESS_LG = "bg-success-200 text-white py-3 px-4 text-title-xl hover:shadow-success-400 hover:bg-success-300 active:bg-success-200",
-  SUCCESS_SM = "bg-success-200 text-white py-2.5 px-4 text-title-lg hover:shadow-success-400 hover:bg-success-300 active:bg-success-200",
-  WARNING_LG = "bg-warning-200 text-black py-3 px-4 text-title-xl hover:shadow-warning-400 hover:bg-warning-300 active:bg-warning-200",
-  WARNING_SM = "bg-warning-200 text-white py-2.5 px-4 text-title-lg hover:shadow-warning-400 hover:bg-warning-300 active:bg-warning-200",
+  PRIMARY_LG = `${DISABLED_STYLE_1} bg-blue-500 text-white py-3 px-4 text-title-xl hover:shadow-blue-700 hover:bg-blue-600 active:bg-blue-500`,
+  PRIMARY_SM = `${DISABLED_STYLE_1} bg-blue-500 text-white py-2.5 px-4 text-title-lg hover:shadow-blue-700 hover:bg-blue-600 active:bg-blue-500`,
+  DANGER_LG = `${DISABLED_STYLE_1} bg-danger-200 text-white py-3 px-4 text-title-xl hover:shadow-danger-400 hover:bg-danger-300 active:bg-danger-200`,
+  DANGER_SM = `${DISABLED_STYLE_1} bg-danger-200 text-white py-2.5 px-4 text-title-lg hover:shadow-danger-400 hover:bg-danger-300 active:bg-danger-200`,
+  SUCCESS_LG = `${DISABLED_STYLE_1} bg-success-200 text-white py-3 px-4 text-title-xl hover:shadow-success-400 hover:bg-success-300 active:bg-success-200`,
+  SUCCESS_SM = `${DISABLED_STYLE_1} bg-success-200 text-white py-2.5 px-4 text-title-lg hover:shadow-success-400 hover:bg-success-300 active:bg-success-200`,
+  WARNING_LG = `${DISABLED_STYLE_1} bg-warning-200 text-black py-3 px-4 text-title-xl hover:shadow-warning-400 hover:bg-warning-300 active:bg-warning-200`,
+  WARNING_SM = `${DISABLED_STYLE_1} bg-warning-200 text-white py-2.5 px-4 text-title-lg hover:shadow-warning-400 hover:bg-warning-300 active:bg-warning-200`,
+  //TODO: Add gradient border buttons
   GRADIENT_1_LG = "",
   GRADIENT_1_SM = "",
   GRADIENT_2_LG = "",
   GRADIENT_2_SM = "",
 }
+
 // TODO: Add button variants
 // ex PRIMARY = "primary classname",
 
@@ -41,7 +48,10 @@ export default function Button({
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={`rounded hover:shadow-button-hover active:shadow-button-clicked ${className} ${variant}`}
+      className={`rounded 
+        ${disabled ? "" : "hover:shadow-button-hover active:shadow-button-clicked"} 
+        ${className} 
+        ${variant}`}
     >
       {label}
     </button>
