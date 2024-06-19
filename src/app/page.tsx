1912072d8f06cff4e3860f01d6588b72c81a3221
Milosz Kryzia <130119742+miloszkryzia1@users.noreply.github.com>
2024-06-18 22:38:46 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Textarea from "@/components/input/Textarea";
 
 export default function Page() {
   return (
-    <div className="text-white p-4 h-screen w-full flex items-center justify-center bg-white dark:bg-dark-400">
+    <div className="text-white p-4 h-screen w-full flex items-center justify-center bg-dark-400">
       <h1 className="text-heading-xl bg-2cg-4 text-transparent inline-block bg-clip-text">
         Hello, world!
       </h1>
