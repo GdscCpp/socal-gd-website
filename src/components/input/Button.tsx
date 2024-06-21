aefdf87fdc06ded7a689f1e1f5c61466e3da00e3
miloszkryzia1 <milosz.kryzia@gmail.com>
2024-06-20 20:18:06 -0700

    remove repeating properties from variant strings in Butto and other minor changes

--- src/components/input/Button.tsx
+++ src/components/input/Button.tsx
@@ -1,16 +1,16 @@
 export enum BUTTON_VARIANTS {
-  PRIMARY_LG = "bg-blue-500 text-white py-3 px-4 text-title-xl hover:shadow-blue-700 hover:bg-blue-600 active:bg-blue-500",
-  PRIMARY_SM = "bg-blue-500 text-white py-2.5 px-4 text-title-lg hover:shadow-blue-700 hover:bg-blue-600 active:bg-blue-500",
-  DANGER_LG = "bg-danger-200 text-white py-3 px-4 text-title-xl hover:shadow-danger-400 hover:bg-danger-300 active:bg-danger-200",
-  DANGER_SM = "bg-danger-200 text-white py-2.5 px-4 text-title-lg hover:shadow-danger-400 hover:bg-danger-300 active:bg-danger-200",
-  SUCCESS_LG = "bg-success-200 text-white py-3 px-4 text-title-xl hover:shadow-success-400 hover:bg-success-300 active:bg-success-200",
-  SUCCESS_SM = "bg-success-200 text-white py-2.5 px-4 text-title-lg hover:shadow-success-400 hover:bg-success-300 active:bg-success-200",
-  WARNING_LG = "bg-warning-200 text-black py-3 px-4 text-title-xl hover:shadow-warning-400 hover:bg-warning-300 active:bg-warning-200",
-  WARNING_SM = "bg-warning-200 text-white py-2.5 px-4 text-title-lg hover:shadow-warning-400 hover:bg-warning-300 active:bg-warning-200",
-  GRADIENT_1_LG = "bg-4cg-2 text-white py-0.5 px-0.5 text-title-xl shadow-gradient-button-default shadow-green-500 hover:shadow-green-500 active:brightness-75",
-  GRADIENT_1_SM = "bg-4cg-2 text-white py-0.5 px-0.5 text-title-lg shadow-gradient-button-default shadow-green-500 hover:shadow-green-500 active:brightness-75",
-  GRADIENT_2_LG = "bg-4cg-1 text-white py-0.5 px-0.5 text-title-xl hover:shadow-yellow-500 active:opacity-75",
-  GRADIENT_2_SM = "bg-4cg-1 text-white py-0.5 px-0.5 text-title-lg hover:shadow-yellow-500 active:opacity-75",
+  PRIMARY_LG = "bg-blue-500 text-white py-3 text-title-xl hover:shadow-blue-700 hover:bg-blue-600 active:bg-blue-500",
+  PRIMARY_SM = "bg-blue-500 text-white py-2.5 text-title-lg hover:shadow-blue-700 hover:bg-blue-600 active:bg-blue-500",
+  DANGER_LG = "bg-danger-200 text-white py-3 text-title-xl hover:shadow-danger-400 hover:bg-danger-300 active:bg-danger-200",
+  DANGER_SM = "bg-danger-200 text-white py-2.5 text-title-lg hover:shadow-danger-400 hover:bg-danger-300 active:bg-danger-200",
+  SUCCESS_LG = "bg-success-200 text-white py-3 text-title-xl hover:shadow-success-400 hover:bg-success-300 active:bg-success-200",
+  SUCCESS_SM = "bg-success-200 text-white py-2.5 text-title-lg hover:shadow-success-400 hover:bg-success-300 active:bg-success-200",
+  WARNING_LG = "bg-warning-200 text-black py-3 text-title-xl hover:shadow-warning-400 hover:bg-warning-300 active:bg-warning-200",
+  WARNING_SM = "bg-warning-200 text-white py-2.5 text-title-lg hover:shadow-warning-400 hover:bg-warning-300 active:bg-warning-200",
+  GRADIENT_1_LG = "bg-4cg-2 text-title-xl shadow-gradient-button-default shadow-green-500 hover:shadow-green-500 active:brightness-75",
+  GRADIENT_1_SM = "bg-4cg-2 text-title-lg shadow-gradient-button-default shadow-green-500 hover:shadow-green-500 active:brightness-75",
+  GRADIENT_2_LG = "bg-4cg-1 text-title-xl hover:shadow-yellow-500 active:opacity-75",
+  GRADIENT_2_SM = "bg-4cg-1 text-title-lg hover:shadow-yellow-500 active:opacity-75",
 }
 
 interface ButtonProps {
@@ -30,19 +30,24 @@ export default function Button({
   className,
   variant,
 }: ButtonProps) {
-  //gradient button
-  if (
+  //Check if gradient button
+  const isGradient =
     variant == BUTTON_VARIANTS.GRADIENT_1_LG ||
     variant == BUTTON_VARIANTS.GRADIENT_1_SM ||
     variant == BUTTON_VARIANTS.GRADIENT_2_LG ||
-    variant == BUTTON_VARIANTS.GRADIENT_2_SM
-  )
+    variant == BUTTON_VARIANTS.GRADIENT_2_SM;
+
+  //gradient button
+  if (isGradient)
     return (
       <button
         type={type}
         onClick={onClick}
         disabled={disabled}
         className={`rounded-lg
+          text-white
+          px-0.5
+          py-0.5
           transition
           duration-200
           ease-in-out
@@ -67,6 +72,7 @@ export default function Button({
         onClick={onClick}
         disabled={disabled}
         className={`rounded-lg 
+          px-4
           transition
           duration-200
           ease-in-out
