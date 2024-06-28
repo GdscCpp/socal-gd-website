9f37572ac2459fd0337c38ccf3b56df6c4d6e965
Valen DeLeon <valeriedeleon4521@gmail.com>
2024-06-27 20:27:45 -0700

    Uncomments other elements to restore test page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ import Tab, { orientation } from "@/components/Tab";
 export default function Page() {
   return (
     <div className="text-white p-4 h-screen w-full flex items-center justify-center bg-dark-400">
-      {/* <h1 className="text-heading-xl bg-2cg-4 text-transparent inline-block bg-clip-text">
+      <h1 className="text-heading-xl bg-2cg-4 text-transparent inline-block bg-clip-text">
         Hello, world!
-      </h1> */}
+      </h1>
 
       {/* DIV oF TABS */}
       <div className="flex flex-col items-center space-y-3">
@@ -395,7 +395,7 @@ export default function Page() {
       </div>
 
       {/* just showing all the buttons here, delete later*/}
-      {/* <div className="flex flex-col space-y-3 pr-3">
+      <div className="flex flex-col space-y-3 pr-3">
         <Button
           label="Test"
           type="button"
@@ -472,22 +472,22 @@ export default function Page() {
           disabled={true}
           variant={BUTTON_VARIANTS.GRADIENT_2_LG}
         />
-      </div> */}
+      </div>
 
       {/* showing all chips, delete later */}
-      {/* <Chip label="test" variant={CHIP_VARIANTS.TRANSPARENT} />
+      <Chip label="test" variant={CHIP_VARIANTS.TRANSPARENT} />
       <Chip label="test" variant={CHIP_VARIANTS.SOLID} />
       <Chip label="test" variant={CHIP_VARIANTS.TRANSPARENT} disabled={true} />
-      <Chip label="test" variant={CHIP_VARIANTS.SOLID} disabled={true} /> */}
+      <Chip label="test" variant={CHIP_VARIANTS.SOLID} disabled={true} />
 
-      {/* <Checkbox label="test" checked={true} onChange={() => {}} />
+      <Checkbox label="test" checked={true} onChange={() => {}} />
       <Input label="test" type="text" value="test" onChange={() => {}} />
       <Radio label="test" checked={true} onChange={() => {}} />
       <Switch label="test" checked={true} onChange={() => {}} />
       <Textarea label="test" value="test" onChange={() => {}} />
       <Avatar src="https://avatars.githubusercontent.com/u/472312" alt="test" />
 
-      <Tooltip label="test" /> */}
+      <Tooltip label="test" />
     </div>
   );
 }
