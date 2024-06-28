f62d1105b4a9adf1308e107dd116402274135aff
yz <youssef.zerroug1@gmail.com>
2024-06-27 22:09:58 -0700

    input and text area styling changes

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -6,6 +6,8 @@ interface InputProps {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   disabled?: boolean;
+  required?: boolean;
+  size?: "small" | "medium" | "large";
   className?: string;
 }
 
@@ -15,24 +17,55 @@ export default function Input({
   value,
   onChange,
   disabled = false,
+  required = false,
+  size = "medium",
   className,
 }: InputProps) {
   const [isFocused, setIsFocused] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-  const gradientClass = isFocused ? "border-gradient" : "border-gray-300";
+  const fontSizes = {
+    small: "text-sm",
+    medium: "text-base",
+    large: "text-lg",
+  };
+
+  const colorScheme = {
+    default: "bg-white dark:bg-dark-400 text-gray-300 border-gray-300",
+    hover: "border-gradient",
+    focus:
+      "text-gray-700 dark:text-gray-300 border-gradient dark:text-gray-100",
+    disabled: "bg-gray-200 dark:bg-gray-400 border-gray-400 dark:bg-gray-800",
+  };
+
+  const theme = disabled
+    ? colorScheme.disabled
+    : isHovered
+      ? colorScheme.hover
+      : isFocused
+        ? colorScheme.focus
+        : colorScheme.default;
 
   return (
-    <div className={`relative ${className} ${disabled ? "opacity-50" : ""}`}>
-      <label className="block text-gray-700 dark:text-gray-300 mb-1">
-        {label}
+    <div
+      className={`relative ${className} ${disabled ? "opacity-50" : ""}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <label
+        className={`block ${fontSizes[size]} text-gray-700 dark:text-gray-300 mb-1`}
+      >
+        {label} {required && <span className="text-red-500">*</span>}
       </label>
       <div
-        className={`relative flex items-center ${gradientClass} py-2 px-3 rounded-md border-2 transition-all`}
+        className={`relative ${theme} flex items-center py-2 px-3 rounded-md border-2 transition-all
+        ${required && "border-red-500"}`}
       >
         <input
           type={type}
-          className="flex-1 bg-transparent focus:outline-none"
-          value={value}
+          className={`${fontSizes[size]} ${theme} flex-1 bg-transparent focus:outline-none
+          ${disabled ? "cursor-not-allowed" : "cursor-pointer"}`}
+          placeholder={value}
           onChange={onChange}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
--- src/components/input/Textarea.tsx
+++ src/components/input/Textarea.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface TextareaProps {
   label: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   disabled?: boolean;
+  required?: boolean;
+  size?: "small" | "medium" | "large";
   className?: string;
 }
 
@@ -13,19 +15,51 @@ export default function Textarea({
   value,
   onChange,
   disabled = false,
+  required = false,
+  size = "medium",
   className,
 }: TextareaProps) {
   const [isFocused, setIsFocused] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const gradientClass = isFocused ? "border-gradient" : "border-gray-300";
 
+  const fontSizes = {
+    small: "text-sm",
+    medium: "text-base",
+    large: "text-lg",
+  };
+
+  const colorScheme = {
+    default: "bg-white dark:bg-dark-400 text-gray-300 border-gray-300",
+    hover: "border-gradient",
+    focus:
+      "text-gray-700 dark:text-gray-300 border-gradient dark:text-gray-100",
+    disabled: "bg-gray-200 dark:bg-gray-400 border-gray-400 dark:bg-gray-800",
+  };
+
+  const theme = disabled
+    ? colorScheme.disabled
+    : isHovered
+      ? colorScheme.hover
+      : isFocused
+        ? colorScheme.focus
+        : colorScheme.default;
+
   return (
-    <div className={`relative ${className} ${disabled ? "opacity-50" : ""}`}>
-      <label className="block text-gray-700 dark:text-gray-300 mb-1">
-        {label}
+    <div
+      className={`${className} ${disabled ? "opacity-50" : ""}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <label
+        className={`block ${fontSizes[size]} text-dark-400 dark:text-gray-100 font-medium rounded-md`}
+      >
+        {label} {required && <span className="text-red-500">*</span>}
       </label>
       <div
-        className={`relative ${gradientClass} rounded-md border-2 transition-all`}
+        className={`${theme} relative ${gradientClass} rounded-md border-2 transition-all w-80
+      ${required && "border-red-500"}`}
       >
         <textarea
           placeholder={value}
@@ -33,7 +67,8 @@ export default function Textarea({
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
           disabled={disabled}
-          className="w-full h-32 bg-transparent p-3 rounded-md focus:outline-none"
+          className={`w-full ${fontSizes[size]} ${theme} bg-transparent p-3 rounded-md focus:outline-none
+          ${disabled ? "cursor-not-allowed" : "cursor-pointer"}`}
         />
       </div>
     </div>
