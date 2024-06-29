fcb9e4e6c470f17bf5cf68965176853718f88fdc
Valen DeLeon <valeriedeleon4521@gmail.com>
2024-06-29 14:16:52 -0700

    Deletes reference comment

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -149,32 +149,3 @@ function Tab({ text, className, display, rounded, style }: TabProps) {
 }
 
 export default Tab;
-
-/*
-can be deleted later!
-
-        //////////////////////////////
-        // GEN TAB STYLES REFERENCE //
-        //////////////////////////////
-
-        // COLORS //
-            > LIGHT
-                        | BACKGROUND                 |  TEXT/ICON COLOR
-                STYLE 1:| transparent                |  #4F575E
-                STYLE 2:| transparent                |  #FECB33 [secondary/Yellow]
-                STYLE 3:| #F8F5FF                    |  #5693FD [primary/Blue 500] (70% opacity)
-                STYLE 4:| #F8F5FF (50% opacity)      |  #FECB33 [secondary/Yellow]
-                STYLE 5:| #F7F9FD                    |  #A7AEB4
-                STYLE 6:| #5693FD [primary/Blue 500] |  #FFFFFF [surface/White]
-                STYLE 7:| #5693FD [primary/Blue 500] |  #FECB33 [secondary/Yellow]
-            > DARK
-                        | BACKGROUND                              |  TEXT COLOR
-                STYLE 1:| transparent                             |  #CBCFD2
-                STYLE 2:| transparent                             |  #FECB33 [secondary/Yellow]
-                STYLE 3:| #7D49F8 (15% opacity) [opacity/primary] |  #5693FD [primary/Blue 500]
-                STYLE 4:| #7D49F8 (15% opacity) [opacity/primary] |  #FECB33 [secondary/Yellow]
-                STYLE 5:| #212125 [surface/Grey 900]              |  #6B767F
-                STYLE 6:| #5693FD [primary/Blue 500]              |  #FFFFFF [surface/White]
-                STYLE 7:| #5693FD [primary/Blue 500]              |  #FECB33 [secondary/Yellow]
-
-        */
