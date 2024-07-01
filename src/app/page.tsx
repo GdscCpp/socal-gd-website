5aa7dba8c710bcc53e1eb9b2bc5884a331a4d15c
richweich <146883126+richweich@users.noreply.github.com>
2024-07-01 15:07:43 -0700

    Richwei/components (#13)
    * add radio and checkbox components
    
    * styled icons for radio and checks
    
    * added comments and fixed format
    
    * svg componentization
    
    * checked fix for radio and checkbox
    
    * no padding checkbox fix
    
    * final cleanu & svg fix
    
    * homepage reset
    
    ---------
    
    Co-authored-by: blbergo <bryan.l.bergo@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,4 @@
-import Avatar from "@/components/Avatar";
-import Chip, { CHIP_VARIANTS } from "@/components/Chip";
-import Tooltip from "@/components/Tooltip";
-import Button, { BUTTON_VARIANTS } from "@/components/input/Button";
-import Checkbox from "@/components/input/Checkbox";
-import Input from "@/components/input/Input";
-import Radio from "@/components/input/Radio";
-import Switch from "@/components/input/Switch";
-import Textarea from "@/components/input/Textarea";
+"use client";
 
 export default function Page() {
   return (
@@ -14,101 +6,6 @@ export default function Page() {
       <h1 className="text-heading-xl bg-2cg-4 text-transparent inline-block bg-clip-text">
         Hello, world!
       </h1>
-
-      {/* just showing all the buttons here, delete later*/}
-      <div className="flex flex-col space-y-3 pr-3">
-        <Button
-          label="Test"
-          type="button"
-          variant={BUTTON_VARIANTS.PRIMARY_LG}
-        />
-        <Button
-          label="Test"
-          type="button"
-          disabled={true}
-          variant={BUTTON_VARIANTS.PRIMARY_LG}
-        />
-      </div>
-      <div className="flex flex-col space-y-3 pr-3">
-        <Button
-          label="Test"
-          type="button"
-          variant={BUTTON_VARIANTS.SUCCESS_LG}
-        />
-        <Button
-          label="Test"
-          type="button"
-          disabled={true}
-          variant={BUTTON_VARIANTS.SUCCESS_LG}
-        />
-      </div>
-      <div className="flex flex-col space-y-3 pr-3">
-        <Button
-          label="Test"
-          type="button"
-          variant={BUTTON_VARIANTS.DANGER_LG}
-        />
-        <Button
-          label="Test"
-          type="button"
-          disabled={true}
-          variant={BUTTON_VARIANTS.DANGER_LG}
-        />
-      </div>
-      <div className="flex flex-col space-y-3 pr-3">
-        <Button
-          label="Test"
-          type="button"
-          variant={BUTTON_VARIANTS.WARNING_LG}
-        />
-        <Button
-          label="Test"
-          type="button"
-          disabled={true}
-          variant={BUTTON_VARIANTS.WARNING_LG}
-        />
-      </div>
-      <div className="flex flex-col space-y-3 pr-3">
-        <Button
-          label="Test"
-          type="button"
-          variant={BUTTON_VARIANTS.GRADIENT_1_LG}
-        />
-        <Button
-          label="Test"
-          type="button"
-          disabled={true}
-          variant={BUTTON_VARIANTS.GRADIENT_1_LG}
-        />
-      </div>
-      <div className="flex flex-col space-y-3 pr-3">
-        <Button
-          label="Test"
-          type="button"
-          variant={BUTTON_VARIANTS.GRADIENT_2_LG}
-        />
-        <Button
-          label="Test"
-          type="button"
-          disabled={true}
-          variant={BUTTON_VARIANTS.GRADIENT_2_LG}
-        />
-      </div>
-
-      {/* showing all chips, delete later */}
-      <Chip label="test" variant={CHIP_VARIANTS.TRANSPARENT} />
-      <Chip label="test" variant={CHIP_VARIANTS.SOLID} />
-      <Chip label="test" variant={CHIP_VARIANTS.TRANSPARENT} disabled={true} />
-      <Chip label="test" variant={CHIP_VARIANTS.SOLID} disabled={true} />
-
-      <Checkbox label="test" checked={true} onChange={() => {}} />
-      <Input label="test" type="text" value="test" onChange={() => {}} />
-      <Radio label="test" checked={true} onChange={() => {}} />
-      <Switch label="test" checked={true} onChange={() => {}} />
-      <Textarea label="test" value="test" onChange={() => {}} />
-      <Avatar src="https://avatars.githubusercontent.com/u/472312" alt="test" />
-
-      <Tooltip label="test" />
     </div>
   );
 }
