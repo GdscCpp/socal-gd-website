c22cde558a33216acdd277734a4134b080a2cbb0
Mark <mark.garcia4@laverne.edu>
2024-07-16 12:23:31 -0700

    merge conflict 7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 "use client";
-
-import End from "./homepage/End";
-import Header from "./homepage/Header";
-import Middle from "./homepage/Middle";
-
 export default function Page() {
   return (
-    <div className="">
-      <Header />
-      <Middle />
-      <End />
-
-      <h1 className="text-heading-xl bg-2cg-4 text-transparent  bg-clip-text">
-        {/* Hello, world! */}
+    <div className="text-white p-4 h-screen w-full flex items-center justify-center bg-dark-400">
+      <h1 className="text-heading-xl bg-2cg-4 text-transparent inline-block bg-clip-text">
+        Hello, world!
       </h1>
     </div>
   );
