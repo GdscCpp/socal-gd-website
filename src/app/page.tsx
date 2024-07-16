ffa4a35eb23f66a8379629fdc629a8a64c3a6d98
Mark <mark.garcia4@laverne.edu>
2024-07-16 12:04:21 -0700

    merge conflict 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <Header />
       <Middle />
       <End />
-      <div className="text-white p-4 h-screen w-full flex items-center justify-center">
+      <div className="text-white p-4 h-screen w-full flex items-center justify-center bg-dark-400">
         <h1 className="text-heading-xl bg-2cg-4 text-transparent inline-block bg-clip-text">
           Hello, world!
         </h1>
