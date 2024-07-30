8caabe1015e3073e6eab9f8b1d10b81c6fff8186
damiankryzia71 <damiankryzia71@gmail.com>
2024-07-30 16:24:18 -0700

    add DataResponse interface, change error handling

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,5 +1,11 @@
+//Data response object
+interface DataResponse {
+  status: number;
+  data: EventData[];
+}
+
 //Event object
-interface EventInfo {
+interface EventData {
   id: number;
   title: string;
   description: string;
@@ -11,50 +17,44 @@ interface EventInfo {
   chapterTitle: string;
 }
 
-//Error response object
-interface ErrorResponse {
-  message: string;
-  cause: any;
-}
-
 /*
 * Fetches upcoming GDSC events.
 *
 * @param {boolean} orderByProximity?   Optional: If true, events are ordered by proximity based on the client's location.
 * @param {number} proximity?           Optional: If specified, only events in the given proximity from the client's location are fetched.
 *
-* @return {Promise<EventInfo[] | ErrorResponse>} An array of EventInfo objects that hold information on upcoming events, or an error response object if an error occurs.
+* @return {Promise<DataResponse>} Data response object that contains the response status and the event data returned.
 */
 export async function getEventsGdsc(
   orderByProximity?: boolean,
   proximity?: number,
-): Promise<EventInfo[] | ErrorResponse> {
-  try {
-    const url: string = `https://gdsc.community.dev/api/search/?result_types=upcoming_event${orderByProximity ? "&order_by_proximity=true" : ""}${proximity ? `&proximity=${proximity}` : ""}`;
-    const response = await fetch(url);
-    const json = await response.json();
-    const events: Array<EventInfo> = json["results"].map((result: any) => {
-      const event: EventInfo = {
-        id: result.id,
-        title: result.title,
-        description: result.description_short,
-        picture: result.picture,
-        city: result.city,
-        date: new Date(result.start_date),
-        eventType: result.event_type_title,
-        url: result.url,
-        chapterTitle: result.chapter.title,
-      };
-      return event;
-    });
-    return events;
-  } catch (error: any) {
-    const response: ErrorResponse = {
-      message: error.message,
-      cause: error.cause,
-    };
-    return response;
+): Promise<DataResponse> {
+  const url: string = `https://gdsc.community.dev/api/search/?result_types=upcoming_event${orderByProximity ? "&order_by_proximity=true" : ""}${proximity ? `&proximity=${proximity}` : ""}`;
+  const response = await fetch(url);
+  const json = await response.json();
+
+  if (!json["results"]) {
+    throw new Error("404 (Not Found)");
   }
+
+  const events: Array<EventData> = json["results"].map((result: any) => {
+    const event: EventData = {
+      id: result.id,
+      title: result.title,
+      description: result.description_short,
+      picture: result.picture,
+      city: result.city,
+      date: new Date(result.start_date),
+      eventType: result.event_type_title,
+      url: result.url,
+      chapterTitle: result.chapter.title,
+    };
+    return event;
+  });
+  return {
+    status: response.status,
+    data: events
+  };
 }
 
 /*
@@ -63,36 +63,36 @@ export async function getEventsGdsc(
 * @param {boolean} orderByProximity?   Optional: If true, events are ordered by proximity based on the client's location.
 * @param {number} proximity?           Optional: If specified, only events in the given proximity from the client's location are fetched.
 *
-* @return {Promise<EventInfo[] | ErrorResponse>} An array of EventInfo objects that hold information on upcoming events, or an error response object if an error occurs.
+* @return {Promise<DataResponse>} Data response object that contains the response status and the event data returned.
 */
 export async function getEventsGDG(
   orderByProximity?: boolean,
   proximity?: number,
-): Promise<EventInfo[] | ErrorResponse> {
-  try {
-    const url: string = `https://gdg.community.dev/api/search/?result_types=upcoming_event${orderByProximity ? "&order_by_proximity=true" : ""}${proximity ? `&proximity=${proximity}` : ""}`;
-    const response = await fetch(url);
-    const json = await response.json();
-    const events: Array<EventInfo> = json["results"].map((result: any) => {
-      const event: EventInfo = {
-        id: result.id,
-        title: result.title,
-        description: result.description_short,
-        picture: result.picture,
-        city: result.city,
-        date: new Date(result.start_date),
-        eventType: result.event_type_title,
-        url: result.url,
-        chapterTitle: result.chapter.title,
-      };
-      return event;
-    });
-    return events;
-  } catch (error: any) {
-    const response: ErrorResponse = {
-      message: error.message,
-      cause: error.cause,
-    };
-    return response;
+): Promise<DataResponse> {
+  const url: string = `https://gdg.community.dev/api/search/?result_types=upcoming_event${orderByProximity ? "&order_by_proximity=true" : ""}${proximity ? `&proximity=${proximity}` : ""}`;
+  const response = await fetch(url);
+  const json = await response.json();
+
+  if (!json["results"]) {
+    throw new Error("404 (Not Found)");
   }
+
+  const events: Array<EventData> = json["results"].map((result: any) => {
+    const event: EventData = {
+      id: result.id,
+      title: result.title,
+      description: result.description_short,
+      picture: result.picture,
+      city: result.city,
+      date: new Date(result.start_date),
+      eventType: result.event_type_title,
+      url: result.url,
+      chapterTitle: result.chapter.title,
+    };
+    return event;
+  });
+  return {
+    status: response.status,
+    data: events
+  };
 }
