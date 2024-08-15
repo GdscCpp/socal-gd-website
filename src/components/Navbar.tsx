dc3fd801efff4f3866244d282dbb124e2ad0321b
blbergo <bryan.l.bergo@gmail.com>
2024-08-15 10:29:36 -0700

    merge fix 1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,19 +51,12 @@ export default function Navbar() {
               className="inline-flex justify-center items-center w-full text-body-xl text-white hover:text-gray-300 transition-colors duration-200"
             >
               Pathways
-              <svg
-                className={`ml-2 -mr-1 h-5 w-5 transform transition-transform duration-200 ${isOpen ? "rotate-180" : ""}`}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <Image
+                src={"/images/arrow-down.png"}
+                alt={"arrow"}
+                width={30}
+                height={30}
+              />
             </button>
 
             {isOpen && (
@@ -89,20 +82,6 @@ export default function Navbar() {
                       onClick={() => setIsOpen(false)}
                     >
                       <span>{pathway.name}</span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M9 5l7 7-7 7"
-                        />
-                      </svg>
                     </Link>
                   ))}
                 </div>
